b83c4a427b2267b00c0a05dfcc5f6b16f4534d76
Deepak Singh <codekeep18feb@gmail.com>
2024-10-09 16:12:32 +0000

    updated to trigger the pipeline

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
-
+        
 def test_hello(client):
     rv = client.get('/')
     assert rv.data == b"Hello, World!"
